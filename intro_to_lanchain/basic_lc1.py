3931d99040b5a6603505914f946622199863724d
meghana <bandaru.saimeghana@gmail.com>
2026-06-12 20:11:18 +0530

    Update basic_lc1.py

--- intro_to_lanchain/basic_lc1.py
+++ intro_to_lanchain/basic_lc1.py
@@ -4,19 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 model=ChatGoogleGenerativeAI(model='gemini-2.5-flash',convert_system_message_to_human=True,api_key=os.getenv('Gemini_api_key'))
 
+#model creation
 from langchain_core.prompts import ChatPromptTemplate
 prompt=ChatPromptTemplate.from_messages([
     ('system','you help out the user by providing ans in json format'),
     ('user','what does {que} mean')
 ])
-
-
-# parser
-from langchain_core.output_parsers import JsonOutputParser
-parser=JsonOutputParser()
-
 #user input 
 question=input("ask anything you want")
+#LCEL
 chain=prompt | model
+# final response
 response=chain.invoke({'que':question})
 print(response.content)
